4dfb5d278ca9a938a465c44cdba15c50fec51e68
mdrayhan3322 <mdrayhan260225@gmail.com>
2025-10-21 19:08:31 +0600

    my furst urls views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+  return HttpResponse("welcome to the task management system")
+def contact(request):
+  return HttpResponse("<h1 style='color:red'>welcome to the contact pase <h1/>")
